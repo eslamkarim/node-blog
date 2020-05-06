81e2012e59bcdb35797b9a14ce790e2a758d13d0
Eslam karim gaber <eslamkarim222@gmail.com>
2020-05-06 23:51:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const express = require('express');
-const usersRouter = require('./routes/usersRouter')
-const postsRouter = require('./routes/postsRouter')
-const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 
-mongoose.connect('mongodb://localhost:27017/blog', {useNewUrlParser: true, useUnifiedTopology: true });
 
 var app = express();
 
@@ -13,9 +9,10 @@ var app = express();
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(cookieParser())
-app.use('/users', usersRouter)
-app.use('/posts', postsRouter)
 
+app.get('/', function(req, resp) {
+    return resp.status(200).send("This is working hi!")
+});
 app.listen(3000, function () {
   console.log('listening on port 3000!');
 });
